8dbdcb254af0b107a203ada223e18adf070871ed
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2025-08-16 22:31:41 -0300

    💄 style: Atualização e adição de novas cores.

--- app/src/main/java/com/example/brainquest/ui/theme/Color.kt
+++ app/src/main/java/com/example/brainquest/ui/theme/Color.kt
@@ -11,5 +11,7 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 // Cores personalizadas
-val PurpleTheme = Color(126, 128, 216)
-val YellowTheme = Color(247, 217, 50)
+val BackgroundColorTheme = Color(0xFFFFFFFF)
+val PurpleTheme = Color(0xFF7E80D8)
+val YellowTheme = Color(0xFFF7D932)
+val GreyTheme = Color(0xFFF3F3F3)
