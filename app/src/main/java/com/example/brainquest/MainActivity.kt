d1c8792ccf6cbd5508c33624d0220e4dcd9d033a
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2025-08-18 14:46:32 -0300

    🗑️ Limpeza de importações não usadas.

--- app/src/main/java/com/example/brainquest/MainActivity.kt
+++ app/src/main/java/com/example/brainquest/MainActivity.kt
@@ -1,25 +1,15 @@
 package com.example.brainquest
 
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.brainquest.ui.auth.AuthScreen
 import com.example.brainquest.ui.theme.BrainQuestTheme
 import dagger.hilt.android.AndroidEntryPoint
