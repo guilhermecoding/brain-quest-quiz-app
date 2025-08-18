0bcf785393282a27f794d979d96bb9dc93b95f9d
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2025-08-18 00:53:59 -0300

    💄 feat: Troca de cor da Barra de Navgeação.

--- app/src/main/java/com/example/brainquest/MainActivity.kt
+++ app/src/main/java/com/example/brainquest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,7 +32,11 @@ class MainActivity : ComponentActivity() {
         // <-- REMOVER ISSO ANTES DO BUILD FINAL --> Mantem a tela acesa
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.light(
+                Color.White.toArgb(), Color.White.toArgb()
+            )
+        )
         setContent {
             BrainQuestTheme {
                 AuthScreen()
